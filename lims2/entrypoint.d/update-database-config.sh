bf2532c9b8feb1f542dcd285c5500b1a487097bb
Weiyi Zhang <weiyi.zhang@geneegroup.com>
2014-07-11 17:05:09 +0800

    Change mysql internal domain name from `db` to `mysql`.

--- lims2/entrypoint.d/update-database-config.sh
+++ lims2/entrypoint.d/update-database-config.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sed -i "s|\(\$config\['root'\]=.*\)://\(genee.*@[^/]*\)/|\1://genee:83719730@db/|" ${LIMS2_DIR}/application/config/database.php
+sed -i "s|\(\$config\['root'\]=.*\)://\(genee.*@[^/]*\)/|\1://genee:83719730@mysql/|" ${LIMS2_DIR}/application/config/database.php
